ee1bf1e191fc05baf5cc673208bc113523450ddb
jaylonia03 <93580526+jaylonia03@users.noreply.github.com>
2026-04-27 16:57:26 +0800

    Change bot token in bot.py
    Updated the bot token for authentication.

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from flask import Flask, request
 
 # ========== CONFIG ==========
-BOT_TOKEN = "8593941443:AAFKvItX1qE0Nc1gQkZrKN-xB19bDMf3rZU"
+BOT_TOKEN = "8593941443:AAF4aKPlw_JhB0txgWt63gG2OJTwkpY4VQI"
 ADMIN_ID = 1975110056
 
 bot = telebot.TeleBot(BOT_TOKEN)
